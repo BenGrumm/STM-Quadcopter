7d8a1078b11c926d6f009fad63b7288518f80d61
BenGrumm <ben.grummitt@outlook.com>
2022-10-06 18:36:08 +0100

    Increase sampling time due to high input impedance

--- Inc/BatteryMeasure4SLiPo.h
+++ Inc/BatteryMeasure4SLiPo.h
@@ -5,6 +5,11 @@
 #include <math.h>
 #include <stdio.h>
 
+/**
+ * Equation 1: RAIN max formula
+ * For error below 1/4 of LSB should use impedance of < 50 kOhm
+ */
+
 // 12 bit value from 0-4095 with 3.3v = 4095 & 0v = 0
 #define ADC_TO_VOLTAGE_VAL 1240.9f
 
